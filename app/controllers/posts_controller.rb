89ded11e69e517e6c9bf6d660344f75776ec5205
DeadFred <f.cliff1@hotmail.com>
2017-10-13 15:49:41 +1000

    added user-only delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,10 +54,12 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
+    if current_user.id == @post.user_id
+      @post.destroy
+      respond_to do |format|
+        format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
